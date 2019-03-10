46eddd3a885e8eab9cfd554c8f308c30857bb5b8
Vitor <vitorgpalves@gmail.com>
2019-03-10 12:12:12 -0300

    Cria a página 404

---
+++ app/wp-content/themes/rest/404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>404</title>
+  <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/style-404.css">
+</head>
+<body>
+  <div class="container">
+  <h1>404</h1>
+  <p>Page not found</p>
+  <a href="/">Back to home</a>
+  </div>
+</body>
+</html>
