4a6aedc0fc15ab18c603aad2dbaeb7765fe07227
Vitor <vitorgpalves@gmail.com>
2019-03-17 12:07:51 -0300

    Adiciona campo personalizado para a localização na página de contato

--- app/wp-content/themes/rest/page-contato.php
+++ app/wp-content/themes/rest/page-contato.php
@@ -8,7 +8,7 @@
 	<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 	<div class="grid-16">
-		<a href="https://www.google.com.br/maps" target="_blank"><img src="<?= get_template_directory_uri(); ?>/img/rest-mapa.jpg" alt="Fachada do Rest"></a>
+		<a href="<?php the_field('link_do_mapa') ?>" target="_blank"><img src="<?php the_field('imagem_do_mapa') ?>" alt="Localização do Rest"></a>
 	</div>
 
 	<div class="grid-1-3 contato-item">
