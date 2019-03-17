4a18de7634199cde957a5615a3f37189b8140bcf
Vitor <vitorgpalves@gmail.com>
2019-03-17 16:33:37 -0300

    Adiciona menu dinâmico ao rodapé em substituição ao estático

--- app/wp-content/themes/rest/footer.php
+++ app/wp-content/themes/rest/footer.php
@@ -1,10 +1,9 @@
 <footer>
   <nav class="nav-footer">
-    <ul>
-      <li class="current_page_item"><a href="/">Menu</a></li>
-      <li><a href="<?php get_site_url(); ?>/sobre">Sobre</a></li>
-      <li><a href="<?php get_site_url(); ?>/contato">Contato</a></li>
-    </ul>
+    <?php
+      $args = array('menu' => 'principal', 'container' => false);
+      wp_nav_menu($args);
+    ?>
   </nav>
 
   <p><?php echo bloginfo('name'); ?> © <?php echo date('Y') ?>. Alguns direitos reservados.</p>
