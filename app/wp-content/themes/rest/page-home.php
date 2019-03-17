59a0c78a251691ebfde78b8639c0448d9451cf89
Vitor <vitorgpalves@gmail.com>
2019-03-17 16:34:54 -0300

    Adiciona campos personalizados ao menu da semana

--- app/wp-content/themes/rest/page-home.php
+++ app/wp-content/themes/rest/page-home.php
@@ -6,60 +6,28 @@
 
 <section class="container">
 	<h2 class="subtitulo"><?php the_title(); ?></h2>
-
-	<div class="menu-item grid-8">
-		<h2>Peixes</h2>
+	
+	<?php for($i=1; $i<=2; $i++){?>
+	<div class="menu-prato grid-8">
+		<h2><?php the_field("titulo_bloco${i}"); ?></h2>
 		<ul>
+			<?php for($j=1; $j<=3; $j++){
+				$prato = get_field("prato_${i}.${j}");
+			?>
 			<li>
-				<span><sup>R$</sup>129</span>
+				<span>
+					<sup>R$</sup><?=$prato["prato_${i}.${j}_preco"]?>
+				</span>
 				<div>
-					<h3>Salmão Grelhado no Forno</h3>
-					<p>Pequenas tiras de salmão feitas no alho e óleo</p>
+					<h3><?=$prato["prato_${i}.${j}_nome"]?></h3>
+					<p><?=$prato["prato_${i}.${j}_descricao"]?></p>
 				</div>
 			</li>
-			<li>
-				<span><sup>R$</sup>89</span>
-				<div>
-					<h3>Sardinha Frita na Cerveja</h3>
-					<p>Sardinhas escolhidas a dedo e fritas em cerveja premium</p>
-				</div>
-			</li>
-			<li>
-				<span><sup>R$</sup>159</span>
-				<div>
-					<h3>Camarão com Catupiry</h3>
-					<p>Grandes camarões grelhados, servidos ao molho de camarão com catupiry</p>
-				</div>
-			</li>
+			<?php } ?>
 		</ul>
 	</div>
-
-	<div class="menu-item grid-8">
-		<h2>Carnes</h2>
-		<ul>
-			<li>
-				<span><sup>R$</sup>129</span>
-				<div>
-					<h3>Picanha Nobre no Alho</h3>
-					<p>Pequenas tiras de salmão feitas no alho e óleo</p>
-				</div>
-			</li>
-			<li>
-				<span><sup>R$</sup>89</span>
-				<div>
-					<h3>Cupim no Bafo</h3>
-					<p>Sardinhas escolhidas a dedo e fritas em cerveja premium</p>
-				</div>
-			</li>
-			<li>
-				<span><sup>R$</sup>159</span>
-				<div>
-					<h3>Hamburger Artesanal Rest</h3>
-					<p>Grandes camarões grelhados, servidos ao molho de camarão com catupiry</p>
-				</div>
-			</li>
-		</ul>
-	</div>
+	<?php } ?>
+	
 
 </section>
 
